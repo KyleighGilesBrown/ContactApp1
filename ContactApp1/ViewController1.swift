bfa108f08b7e903566f6cb0086337c1e8a2886c4
Kyleigh Giles-Brown <kyleighgilesbrown@gmail.com>
2026-03-30 22:24:27 -0400

    initial commit

---
+++ ContactApp1/ViewController1.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController1.swift
+//  ContactApp1
+//
+//  Created by Kyleigh on 3/30/26.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
